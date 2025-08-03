91adb77e7c51680df89c37b0b799a0e6821e02c7
Juan Diego <mineratminiyon@gmail.com>
2025-08-03 13:04:16 -0500

    UPDATE 2 HECHO EN CASA

--- vocales y ACTIVIDAD EN CLASE.py
+++ vocales y ACTIVIDAD EN CLASE.py
@@ -351,109 +351,257 @@
 # print("Último nombre:", ultimo_nombre)
 
 # if ultimo_nombre == "Marta":
+
 #     print("NOMBRE CORRECTO")
+
 # else:
+
 #     print("NOMBRE DIFERENTE")
         
+#--------------------------------------PROBLEMA_15--------------------------------------------#
+
+# CREA UNA LISTA CON TRES COLORES. CAMBIA EL SEGUNDO COLOR SOLO SI ES IGUAL A "AZUL", Y MUESTRA LA LISTA ACTUALIZADA
+
+
+# lista_colores=[]
+
+# lista_colores_modificada=[]
+
+# pedir_color= input("Ingresa un color: ")
+
+
+# pedir_color2= input("Ingresa otro color: ")
+
+
+# pedir_color3= input("Ingresa otro color: ")
+
+
+# pedir_color_repuesto= input("Ingresa otro color de repuesto: ")
+
+# lista_colores.append(pedir_color)
+# lista_colores.append(pedir_color2)
+# lista_colores.append(pedir_color3)
+
+# print(lista_colores)
+
+# lista_colores_modificada.append(pedir_color)
+# lista_colores_modificada.append(pedir_color_repuesto)
+# lista_colores_modificada.append(pedir_color3)
+
+# if pedir_color2 == "Azul" or pedir_color2 == "azul":
+
+#     print(f"Tu lista esta completada y es {lista_colores_modificada}")
+
+# else:
+#     print(f"Tu lista esta completada y es {lista_colores}")
+
+
+
+#--------------------------------------PROBLEMA_16--------------------------------------------#
+
+# CREA UNA TUPLA CON LOS VALORES (5, 8, 12, 20). SI EL PRIMER VALOR ES MENOR QUE EL ÚLTIMO, MUESTRA “ORDEN ASCENDENTE”, SI NO, “ORDEN DESCENDENTE”
+
+        
+# tupla_valores= (5, 8, 12, 20)
+
+# if tupla_valores[0] < tupla_valores[3]:
+
+#     print(" TUPLA ASCENDENTE ")
+    
+# else:
+
+#     print("TUPLA DESCENDENTE ")
 
 
 
 
+#--------------------------------------PROBLEMA_17--------------------------------------------#
+
+# DADA LA TUPLA (25, 32, 28), VERIFICA SI EL SEGUNDO VALOR ES MAYOR A 30. SI LO ES, MUESTRA “EDAD MAYOR A 30”, SI NO, “EDAD MENOR O IGUAL A 30”
 
 
+# tupla_dada= (25, 32, 28)
 
+# if tupla_dada[1] > 30: 
 
+#     print("EDAD MAYOR A 30")
 
+# else:
 
-
-
-
+#     print("EDAD MENOR O IGUAL A 3O")
         
 
 
 
 
+#--------------------------------------PROBLEMA_18--------------------------------------------#
+
+# CONVIERTE LA TUPLA (1, 2, 3) A LISTA, CAMBIA EL SEGUNDO VALOR A 10 SOLO SI ES IGUAL A 2, LUEGO VUELVE A CONVERTIRLA EN TUPLA Y MUÉSTRALA
+
+# tupla_a_lista= (1, 2, 3)
+
+# lista= list(tupla_a_lista)
+
+# if lista[1] == 2:
+
+#     lista[1]= 10
+
+#     tupla= tuple(lista)
+
+#     print(f"SU VALOR A SIDO REEMPLAZADO SU TUPLA QUEDARIA ASI {tupla}")
+
+# else:
+
+#     print(f"SU VALOR SIGUE IGUAL Y ES {tupla_a_lista}")
+
+
+
+#--------------------------------------PROBLEMA_19--------------------------------------------#
+
+# DADA LA TUPLA (4, 9), ACCEDE AL SEGUNDO VALOR. SI ES MAYOR QUE 5, MUESTRA “COORDENADA ALTA”, SI NO, “COORDENADA BAJA”
+
+# tupla_dada_predeterminada= (4, 9)
+
+
+# if tupla_dada_predeterminada[1] > 5:
+
+#     print("COORDENADA ALTA")
+
+# else:
+
+#     print("COORDENADA BAJA")
 
 
 
 
 
+#--------------------------------------PROBLEMA_20--------------------------------------------#
+
+# COMPARA SI LAS TUPLAS (3, 4) Y (3, 5) SON IGUALES. SI LO SON, MUESTRA “TUPLAS IGUALES”, SI NO, “TUPLAS DIFERENTES”
 
 
+# tuplas_2= (3, 4)
 
+
+# tuplas_2_1= (3, 5)
+
+# if tuplas_2 == tuplas_2_1:
+
+#     print("TUPLAS IGUALAS")
+
+
+# else:
+
+#     print("TUPLAS DIFERENTES")
 
         
 
 
+#--------------------------------------PROBLEMA_21--------------------------------------------#
+
+# CREA UN DICCIONARIO CON {"NOMBRE": "JUAN", "EDAD": 17}. SI LA EDAD ES MAYOR O IGUAL A 18, MUESTRA “ADULTO”, SI NO, MUESTRA “MENOR DE EDAD”
 
 
+# diccionario_nombres= {
+    
+#     "NOMBRE": "JUAN",
+#     "EDAD": 17
+# }
 
 
+# if diccionario_nombres["EDAD"] >= 18:
 
+#     print("ES UN ADULTO")
 
+# else:
 
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
+#     print("ES UN MENOR DE EDAD")
 
 
 
         
+#--------------------------------------PROBLEMA_22--------------------------------------------#
+
+# CREA UN DICCIONARIO {"NOMBRE": "LUCÍA", "EDAD": 20}. SI LA EDAD ES MAYOR A 18, CAMBIA EL VALOR DE “EDAD” A 21. LUEGO MUESTRA EL DICCIONARIO
+
+
+# diccionario_nombres_2= {
+
+#     "NOMBRE":"LUCIA",
+#     "EDAD":20
+# }
+
+# if diccionario_nombres_2["EDAD"] > 18:
+
+#     diccionario_nombres_2["EDAD"]= 21
+
+#     print(f"EL DICCIONARIO ESTA TERMINADO Y CAMBIADO QUEDARIA ASI: {diccionario_nombres_2}")
+
+# else:
+
+#    print(f"EL DICCIONARIO QUEDA IGUAL NO HUBO CAMBIOS {diccionario_nombres_2}")
+
+
+#--------------------------------------PROBLEMA_23--------------------------------------------#
+
+# CREA UN DICCIONARIO CON {"NOMBRE": "CARLOS"}. SI LA CLAVE “CIUDAD” NO EXISTE, AGRÉGALA CON EL VALOR “BOGOTÁ” Y MUESTRA EL DICCIONARIO
+
+# diccionario_nombres_3={
+
+#     "NOMBRE":"CARLOS"
+# }
+
+# if "CIUDAD" not in diccionario_nombres_3:
+
+#     diccionario_nombres_3["CIUDAD"]= "BOGOTA"
+
+#     print(f"EN EL DICCIONARIO NO ESTABA CIUDAD ASI QUE SE AGREGO Y QUEDARIA ASI: {diccionario_nombres_3}")
+
+
+# else:
+
+#     print(f"CIUDAD SI ESTA EN EL DICCIONARIO Y QUEDARIAS ASI: {diccionario_nombres_3}")
 
 
 
+#--------------------------------------PROBLEMA_24--------------------------------------------#
+
+# DADO EL DICCIONARIO {"PRODUCTO": "PAN", "PRECIO": 1200}, VERIFICA SI LA CLAVE “PRECIO” EXISTE. SI EXISTE, MUESTRA SU VALOR, SI NO, MUESTRA “NO HAY PRECIO”
+
+
+# diccionario_producto={
+
+#     "PRODUCTO": "PAN",
+#     "PRECIO": 1200
+# }
+
+# if "PRECIO" in diccionario_producto:
+
+#     print(f"EL PRECIO SI ESTA EN EL DICCIONARIO Y ESTE ES SU VALOR: {diccionario_producto["PRECIO"]}")
+
+# else:
+
+#     print(f"EL PRECIO NO ESTA EN EL DICCIONARIO ASI QUE NO HAY PRECIO")
 
 
 
+#--------------------------------------PROBLEMA_25--------------------------------------------#
 
+# CREA UN DICCIONARIO CON {"PAN": 1200, "LECHE": 2000}. SI “PAN” ESTÁ EN EL DICCIONARIO, MUESTRA SU PRECIO; SI NO, MUESTRA “PRODUCTO NO DISPONIBLE”
 
+diccionario_productos_2={
 
+    "PAN": 1200,
+    "LECHE": 2000
+}
 
+if "PAN" in diccionario_productos_2:
 
+    print(f"EL VALOR DEL PAN ES: {diccionario_productos_2["PAN"]}")
 
+else:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print("PRODUCTO NO DISPONIBLE EN EL DICCIONARIO ")
 
 
 
